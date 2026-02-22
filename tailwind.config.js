ce8a8921851e507929d42f78a847eba05a15ca5f
Camilo Canclini <camilocanclini@gmail.com>
2026-02-22 20:26:22 -0300

    deleting an unused plugin in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,20 +40,8 @@ export default {
       },
     },
   },
-  plugins: [addVariablesForColors],
+  //plugins: [addVariablesForColors],
   corePlugins: {
     scrollBehavior: true,
   },
-}
-
-// This plugin adds each Tailwind color as a global CSS variable, e.g. var(--gray-200).
-function addVariablesForColors({ addBase, theme }) {
-  let allColors = flattenColorPalette(theme("colors"));
-  let newVars = Object.fromEntries(
-    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
-  );
- 
-  addBase({
-    ":root": newVars,
-  });
 }
